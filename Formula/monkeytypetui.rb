0dab532b15b545837392d1a729e001cac2ba17d7
sharann-del <sharannmanojkumar@gmail.com>
2026-02-06 22:33:29 +0530

    Initial monkeytypetui formula

---
+++ Formula/monkeytypetui.rb
@@ -0,0 +1,17 @@
+class Monkeytypetui < Formula
+  desc "Monkeytype-inspired terminal typing test"
+  homepage "https://github.com/sharann-del/monkeytypetui"
+  url "https://registry.npmjs.org/monkeytypetui/-/monkeytypetui-1.0.0.tgz"
+  sha256 "197005d3362f46a3c18703b6361299177bd150580d89e51a3aac367506613ff4"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+  end
+
+  test do
+    system "#{bin}/monkeytypetui", "--help"
+  end
+end
