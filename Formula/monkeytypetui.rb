742749088056130ecde8e8813bf7b13f84e344a0
sharann-del <sharannmanojkumar@gmail.com>
2026-02-06 23:04:24 +0530

    Update monkeytypetui to 1.1.0

--- Formula/monkeytypetui.rb
+++ Formula/monkeytypetui.rb
@@ -1,8 +1,8 @@
 class Monkeytypetui < Formula
   desc "Monkeytype-inspired terminal typing test"
   homepage "https://github.com/sharann-del/monkeytypetui"
-  url "https://registry.npmjs.org/monkeytypetui/-/monkeytypetui-1.0.0.tgz"
-  sha256 "197005d3362f46a3c18703b6361299177bd150580d89e51a3aac367506613ff4"
+  url "https://registry.npmjs.org/monkeytypetui/-/monkeytypetui-1.1.0.tgz"
+  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
   license "MIT"
 
   depends_on "node"
